6d80d25598b5d979e69dcc2288903aeca85af6c7
Abhijeet Burman <abhiburman20@gmail.com>
2020-08-19 00:38:47 +0530

    block-BJaabw

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,8 +11,8 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
-  // Your code
+function minToSec(minutes = 0) {
+  return `${minutes*60} sec`
 }
 // - Execute the function with required parameter
 
@@ -26,8 +26,8 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
-  // Your code
+function isInRange(num1,num2,n) {
+  return (num1 < n && num2 > n) ? true : false;
 }
 // - Execute the function with required parameter
 
@@ -49,8 +49,17 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
-  // Your code
+function calculateBMI(weight = 0, height = 1) {
+  let bmi = weight / (height * height);
+  if (bmi < 18.5){
+    return `Underweight`;
+  } else if (18.5 < bmi > 24.9) {
+    return `Normal weight`;
+  } else if (25 < bmi > 29.9) {
+    return `Overweight`;
+  } else {
+    return `Obese`
+  }
 }
 
 /* 3. appropiateDrinks
@@ -64,8 +73,16 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
-  // Your code
+function appropiateDrinks(age) {
+  if ( age < 14) {
+    return "drink fruit juice";
+  } else if ( age < 18) {
+    return "drink soda";
+  } else if (age < 21) {
+    return "drink fruit-flavored beer";
+  } else {
+    return "drink throat-piercing vodka";
+  }
 }
 
 /* 4. Add two numers or string
@@ -79,10 +96,15 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
-  // Your code
+function sum(input1, input2) {
+  if (isNaN(input2) == true && isNaN(input2) == true){ 
+    return input1+input2;
+  } else if (isNaN(input1) == false && isNaN(input2) == false) {
+      return input1+input2;
+  } else {
+    return "Enter valid values";
+  }
 }
-
 // Function Test
 sum(2, 4); // 4
 sum('Arya', 'Stark'); // "Arya Stark"
